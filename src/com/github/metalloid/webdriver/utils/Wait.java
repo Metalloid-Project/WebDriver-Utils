342dfb7245d113d8babee4e8a718d1c7a1fa5910
rafal_laskowski <rafal.laskowski@intive.com>
2020-04-22 09:20:36 +0200

    - Added default timeout duration - 5 seconds - Timeout can be overriden with `withTimeout(Duration duration);` method - Added a possbility to NOT throw TimeoutException with `withoutException(true);` method - Previous methods marked as @Deprecated

--- src/com/github/metalloid/webdriver/utils/Wait.java
+++ src/com/github/metalloid/webdriver/utils/Wait.java
@@ -14,6 +14,8 @@ public class Wait extends Utility {
 	private List<Class<? extends Throwable>> exceptionsToIgnore = new ArrayList<>();
 	private Duration duration = Duration.ofMillis(500);
 	private String exceptionInfo;
+	private Duration timeout = Duration.ofSeconds(5);
+	private boolean throwException = true;
 
 	public Wait(WebDriver driver) {
 		super(driver);
@@ -29,11 +31,28 @@ public class Wait extends Utility {
 		return this;
 	}
 
+	@Deprecated
 	public Wait setExceptionMessage(String message) {
 		this.exceptionInfo = message;
 		return this;
 	}
 
+	public Wait withExceptionMessage(String message) {
+		this.exceptionInfo = message;
+		return this;
+	}
+
+	public Wait withTimeout(Duration timeout) {
+		this.timeout = timeout;
+		return this;
+	}
+
+	public Wait withoutException(boolean withoutException) {
+		throwException = !withoutException;
+		return this;
+	}
+
+	@Deprecated
 	public <T> T until(boolean throwTimeout, int timeout, ExpectedCondition<T> condition) {
 		try {
 			WebDriverWait wait = new WebDriverWait(driver, timeout);
@@ -58,16 +77,38 @@ public class Wait extends Utility {
 		}
 	}
 
+	@Deprecated
 	public <T> T until(int timeout, ExpectedCondition<T> condition) {
 		return until(true, timeout, condition);
 	}
 
+	@Deprecated
 	public <T> T until(boolean throwTimeout, ExpectedCondition<T> condition) {
 		return until(throwTimeout, 10, condition);
 	}
 
 	public <T> T until(ExpectedCondition<T> condition) {
-		return until(true, 10, condition);
+		try {
+			WebDriverWait wait = new WebDriverWait(driver, timeout.getSeconds());
+			exceptionsToIgnore.forEach(wait::ignoring);
+			wait.pollingEvery(duration).withTimeout(timeout);
+
+			if (exceptionInfo != null)
+				wait.withMessage(exceptionInfo);
+
+			return wait.until(condition);
+		} catch (TimeoutException e) {
+			if (throwException)
+				throw e;
+			else {
+				return null;
+			}
+		} finally {
+			exceptionsToIgnore = new ArrayList<>();
+			exceptionsToIgnore.add(WebDriverException.class);
+			duration = Duration.ofMillis(500);
+			exceptionInfo = null;
+		}
 	}
 
 	public void sleep(int seconds) {
